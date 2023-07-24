a7ad5484519500da7a8c26f6b25053aaa7c16652
xOlanga <olanga98@gmail.com>
2023-07-24 15:50:18 +0200

    restructure

---
+++ Chatter/HotkeyCaptureDialogPyramid.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Chatter
+{
+    partial class HotkeyCaptureDialogPyramid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HotkeyCaptureDialogPyramid));
+            capturedHotkeyLabelPyramid = new Label();
+            SuspendLayout();
+            // 
+            // capturedHotkeyLabelPyramid
+            // 
+            capturedHotkeyLabelPyramid.AutoSize = true;
+            capturedHotkeyLabelPyramid.Location = new Point(22, 19);
+            capturedHotkeyLabelPyramid.Margin = new Padding(6, 0, 6, 0);
+            capturedHotkeyLabelPyramid.Name = "capturedHotkeyLabelPyramid";
+            capturedHotkeyLabelPyramid.Size = new Size(172, 32);
+            capturedHotkeyLabelPyramid.TabIndex = 0;
+            capturedHotkeyLabelPyramid.Text = "Press any key...";
+            capturedHotkeyLabelPyramid.Click += capturedHotkeyLabelPyramid_Click;
+            // 
+            // HotkeyCaptureDialogPyramid
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(431, 126);
+            Controls.Add(capturedHotkeyLabelPyramid);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(6);
+            MaximizeBox = false;
+            Name = "HotkeyCaptureDialogPyramid";
+            TopMost = true;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label capturedHotkeyLabelPyramid;
+    }
+}
---
+++ Chatter/HotkeyCaptureDialogSpam.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chatter
+{
+    public partial class HotkeyCaptureDialogSpam : Form
+    {
+        public Keys CapturedHotkey { get; private set; }
+        private bool keyPressed = false;
+
+        public HotkeyCaptureDialogSpam()
+        {
+            InitializeComponent();
+            KeyPreview = true; // Set KeyPreview to true to capture key events
+            KeyDown += HotkeyCaptureDialogSpam_KeyDown;
+            FormClosing += HotkeyCaptureDialogSpam_FormClosing;
+
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                // Close the dialog when the Escape key is pressed
+                Close();
+                return true; // Set to true to prevent further processing of the Escape key
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void HotkeyCaptureDialogSpam_KeyDown(object? sender, KeyEventArgs e)
+        {
+            CapturedHotkey = e.KeyCode;
+            capturedHotkeyLabelSpam.Text = " Selected Key: " + e.KeyCode.ToString() + "\n Press ESC to save";
+
+            keyPressed = true;
+        }
+
+        private void HotkeyCaptureDialogSpam_FormClosing(object? sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                if (keyPressed)
+                {
+                    DialogResult = DialogResult.OK;
+
+                }
+                else
+                {
+                    DialogResult = DialogResult.Cancel;
+                }
+            }
+        }
+
+
+        private void HotkeyCaptureDialogSpam_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
